097214bdda5791ed2ae44228b6bee065eca1100a
suatmete9-max <suatmete9@gmail.com>
2026-08-31 22:08:48 +0530

    Remove camera and location permissions from android project

--- android/app/src/main/java/com/ytlove/app/MainActivity.java
+++ android/app/src/main/java/com/ytlove/app/MainActivity.java
@@ -1,5 +1,29 @@
-package com.ytlove.app;
+package com.socialboost.app;
 
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // WebView level permissions block
+        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            // Disable Geolocation/Location popups
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, false, false);
+            }
+
+            // Disable Camera & Microphone requests
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                request.deny();
+            }
+        });
+    }
+}
